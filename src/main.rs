e2ae01586fba26a01cde0828d2c227e1d0ef3066
nadjib <ndjbchch@gmail.com>
2025-03-02 10:22:51 +0100

    the final of  course

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,34 @@ mod types; //import a file
 fn main() {
     // types::main1(); //call fn of file
     let s = String::from("nadjib");
-    print!("length: {}", length_string(&s))
+    // print!("length: {}", length_string(&s))
+    let mut  account = BankAccount{
+        owner: "Ahmed".to_string(),
+        balance: 34250.00,
+    };
+    account.checkAccount();
+    account.withdraw(200.40);
+    account.checkAccount();
 }
 
 fn length_string(s:&String) -> usize {
     s.len()
+}
+
+// borrow and references:
+
+// like you created a type or data structure and added its funcs
+struct BankAccount {
+    owner: String,
+    balance: f64
+}
+
+impl BankAccount {
+    fn checkAccount(&self) {
+        println!("{} has ${} in his account.",self.owner,self.balance)
+    }
+    fn withdraw(&mut self, amount: f64) {
+        println!("withdrawing ${} from the account of {}.", amount, self.owner);
+        self.balance -= amount
+    }
 }
--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+
+
+
 pub fn main1() {
     let bool_var = true;
     println!("let {}",bool_var); //print a var
@@ -10,4 +14,14 @@ pub fn main1() {
     let tuple = ("nadjib",22,true); //tuple contian different types of data with a fixed size
     println!("tuple :{:?}",tuple);
     let slice = &[1,2,3,4,5,6]; 
+    // vectors
+    let mut  _vec = vec!["ahmed", "ali", "karim"];
+    let mut _vec2 : Vec<i32> = vec![1,2,3,4,56,7,8,9];
+    _vec.push("nadjib");
+    _vec2.pop();
+    // hashmap
+    let mut scores = HashMap::new();
+    scores.insert(12, "nadjib");
+    scores.insert(1, "ali");
+    scores.insert(2, "nooh");
 }
